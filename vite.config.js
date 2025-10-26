f75b7b6f90a194478a9262165fb7ed235661c3ef
nongoben <omyim_rabbit4@hotmail.com>
2025-10-26 17:25:42 +0700

    [Add] vite base path

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/stock-management-system",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
